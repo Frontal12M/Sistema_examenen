57818a72a3a86304552fc707a938704372742222
Miguel Flores <flores.iss21@gmail.com>
2023-07-29 22:13:48 -0600

    Mensaje de confirmación

---
+++ src/main/java/com/sistema/examenes/SistemaExamanesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sistema.examenes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaExamanesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaExamanesBackendApplication.class, args);
+	}
+
+}
